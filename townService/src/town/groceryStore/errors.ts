20a1ddc70a9d94441c6080b6ebdb8ddd9f58d98d
jiaying <jiaying021218@icloud.com>
2024-02-29 23:43:18 -0500

    added new error

--- townService/src/town/groceryStore/errors.ts
+++ townService/src/town/groceryStore/errors.ts
@@ -2,3 +2,6 @@
 export const MODIFY_ITEM_QUANTITY_ERROR = 'Modify item quantity must be positive';
 export const NEGATIVE_ITEM_QUANTITY_ERROR = 'Item quantity must be positive';
 export const ITEM_NOT_FOUND_ERROR = 'Item not found';
+
+// Errors for PlayerDatabase
+export const PLAYER_INVENTORY_NOT_FOUND_ERROR = 'Player inventory not found.';
