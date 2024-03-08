eb14504d625bdab92e6544e2fad9c61172896a3d
jiaying <jiaying021218@icloud.com>
2024-03-08 17:59:48 -0500

    new abstract class for player database

---
+++ townService/src/town/commercial/CommercialArea.ts
@@ -0,0 +1,37 @@
+import { BoundingBox, TownEmitter } from '../../types/CoveyTownSocket';
+import InteractableArea from '../InteractableArea';
+import PlayerDatabase from './database/PlayerDatabase';
+
+/**
+ * CommercialArea is an InteractableArea on the map that can host a commercial area.
+ * The commercial area stores a reference of the player database.
+ *
+ * isActive() is a method that checks if the commercial area is active.
+ */
+export default abstract class CommercialArea extends InteractableArea {
+  protected _playerDatabase;
+
+  constructor(
+    id: string,
+    { x, y, width, height }: BoundingBox,
+    townEmitter: TownEmitter,
+    playerDatabase?: PlayerDatabase,
+  ) {
+    // Calls the constructor of the parent class.
+    super(id, { x, y, width, height }, townEmitter);
+
+    // Sets the playerDatabase.
+    if (playerDatabase) {
+      this._playerDatabase = playerDatabase;
+    } else {
+      this._playerDatabase = new PlayerDatabase();
+    }
+  }
+
+  /**
+   * If the grocery store is active.
+   */
+  public get isActive(): boolean {
+    return true;
+  }
+}
