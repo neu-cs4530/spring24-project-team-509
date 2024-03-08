2a16080cc37d984306d62f5ddd12ca28bef70efc
jiaying <jiaying021218@icloud.com>
2024-03-08 17:47:58 -0500

    prettier

--- frontend/src/classes/interactable/GroceryStoreAreaController.ts
+++ frontend/src/classes/interactable/GroceryStoreAreaController.ts
@@ -1,8 +1,8 @@
+// TODO
 // import InteractableAreaController from "./InteractableAreaController";
 // import GroceryStoreItemList from "./GroceryStoreItemList"; // Import the missing class
 // export default class GroceryStoreAreaController extends InteractableAreaController {
 //     private _groceryStoreInventory: GroceryStoreItemList = new GroceryStoreItemList();
-
 //     constructor(area: GroceryStoreArea) {
 //         super(area);
 //         this._groceryStoreInventory = area.groceryStoreInventory;
