4a42869e63b30ee3aa2a81d1147216b9f3da1792
Krishna Singh <70067302+Krishna-coder6111@users.noreply.github.com>
2024-03-20 16:02:32 +0000

    indoors map without the new classes and wall phasing fix

---
+++ frontend/src/components/Town/interactables/GroceryStore/GroceryStoreArea.tsx
@@ -0,0 +1,18 @@
+import { useInteractableAreaController } from "../../../../classes/TownController";
+import useTownController from "../../../../hooks/useTownController";
+import { InteractableID } from "../../../../types/CoveyTownSocket";
+
+export default function GroceryStoreArea({
+    interactableID,
+}): {
+    interactableID: InteractableID;
+}): JSX.Element {
+    const groceryController = useInteractableAreaController<GroceryController>(interactableID);
+
+    const townController = useTownController();
+
+    //player database
+    //checkout items
+    //checkout status - changes when 
+    //menu items - only change when checkout completes
+}
