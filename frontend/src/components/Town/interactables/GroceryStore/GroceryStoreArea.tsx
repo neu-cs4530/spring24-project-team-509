b4887d61554eff4862f45e6c8e02d6f1496ce67d
Krishna Singh <70067302+Krishna-coder6111@users.noreply.github.com>
2024-03-20 19:18:08 +0000

    without frontend

--- frontend/src/components/Town/interactables/GroceryStore/GroceryStoreArea.tsx
+++ frontend/src/components/Town/interactables/GroceryStore/GroceryStoreArea.tsx
@@ -1,18 +1,18 @@
-import { useInteractableAreaController } from "../../../../classes/TownController";
-import useTownController from "../../../../hooks/useTownController";
-import { InteractableID } from "../../../../types/CoveyTownSocket";
+// import { useInteractableAreaController } from "../../../../classes/TownController";
+// import useTownController from "../../../../hooks/useTownController";
+// import { InteractableID } from "../../../../types/CoveyTownSocket";
 
-export default function GroceryStoreArea({
-    interactableID,
-}): {
-    interactableID: InteractableID;
-}): JSX.Element {
-    const groceryController = useInteractableAreaController<GroceryController>(interactableID);
+// export default function GroceryStoreArea({
+//     interactableID,
+// }): {
+//     interactableID: InteractableID;
+// }): JSX.Element {
+//     const groceryController = useInteractableAreaController<GroceryController>(interactableID);
 
-    const townController = useTownController();
+//     const townController = useTownController();
 
-    //player database
-    //checkout items
-    //checkout status - changes when 
-    //menu items - only change when checkout completes
-}
+//     //player database
+//     //checkout items
+//     //checkout status - changes when 
+//     //menu items - only change when checkout completes
+// }
