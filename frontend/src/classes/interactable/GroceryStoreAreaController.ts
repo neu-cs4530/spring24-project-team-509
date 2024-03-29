7b5fe2f95a40e279c809ff79a22caad8fe5bc9b9
jiaying <jiaying021218@icloud.com>
2024-03-29 19:02:10 -0400

    template for cart and fix grocery

--- frontend/src/classes/interactable/GroceryStoreAreaController.ts
+++ frontend/src/classes/interactable/GroceryStoreAreaController.ts
@@ -38,6 +38,7 @@ export default class GroceryStoreAreaController extends InteractableAreaControll
       type: 'GroceryStoreArea',
       totalPrice: this._totalPrice,
       storeInventory: this._storeInventory,
+      // cart: this._cart,
     };
   }
 
